4df29984d43feb19e7dcefc5fb8727fe7c06c21d
Daniel Paschoalinoto <danielpaschoalinoto@gmail.com>
2025-05-05 11:24:18 -0300

    Removendo tab color e setando mais testes

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 import sleep from "./utils/sleep.js";
 import log from "./utils/textformatter.js";
-import { setBackgroundRGB, setWindowTitle, setTabColor } from "./utils/windowFormatter.js";
+import { setBackgroundRGB, setWindowTitle } from "./utils/windowFormatter.js";
 
 // Configurações iniciais
+await sleep(2000);
+await log("Mudando fundo");
 setBackgroundRGB(10, 10, 40);
-setWindowTitle("Teste Visual do WindowFormatter");
-setTabColor(100, 200, 255);
+await sleep(2000);
+
+await log("Mudando Title");
+setWindowTitle("Teste");
+await sleep(2000);
 
 await log("[INICIANDO TESTE DE FORMATAÇÃO DE JANELA...]");
-await sleep(1000);
+await sleep(2000);
 
 await log("1️⃣ Centralizando janela...", "m", "yellow");
 await sleep(2000);
@@ -18,7 +23,6 @@ await log("2", "f", "magenta");
 await sleep(500);
 await log("1", "f", "magenta");
 await sleep(500);
-minimizeWindow();
 await sleep(3000); // Dê tempo para ver que minimizou
 
 await log("3️⃣ Restaurando e maximizando...", "m", "green");
--- utils/windowFormatter.js
+++ utils/windowFormatter.js
@@ -9,9 +9,3 @@ export function setWindowTitle(title) {
   const seq = `\x1b]0;${title}\x07`;
   Deno.stdout.writeSync(new TextEncoder().encode(seq));
 }
-
-export function setTabColor(r, g, b) {
-  const hex = `#${r.toString(16).padStart(2, "0")}${g.toString(16).padStart(2, "0")}${b.toString(16).padStart(2, "0")}`;
-  const seq = `\x1b]633;SetTabColor=${hex}\x1b\\`;
-  Deno.stdout.writeSync(new TextEncoder().encode(seq));
-}
